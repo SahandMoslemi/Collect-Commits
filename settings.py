e8fe78b0d2fb85f27966b004aa378d0080a2babd
Sahand <SahandMoslemi@gmail.com>
2023-12-11 17:39:22 +0300

    requirements updated

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 FIRST_FILE_INDEX = 0
 LAST_FILE_INDEX = 5
-TOKEN = "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"  # This personal token is expired, replace it with yours.
+TOKEN = "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"  # This personal token is expired, replace it with yours.
