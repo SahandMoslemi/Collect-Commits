2888c9d1e90d4add5b3d2abf67498f987d7316f4
Sahand <SahandMoslemi@gmail.com>
2023-12-11 20:13:20 +0300

    Update settings.py
    initial NEXTINDEXTOOBSERVE changed.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 TOKEN = "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"  # This personal token is expired, replace it with yours.
 
 FIRST_FILE_INDEX = 0
-LAST_FILE_INDEX = 5
+LAST_FILE_INDEX = 4
 
-NEXT_INDEX_TO_OBSERVE = 3
+NEXT_INDEX_TO_OBSERVE = 0
